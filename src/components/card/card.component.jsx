108d12e1e143c051bae14c5c291c5090037c78f3
Hasan-aga <careerforma@gmail.com>
2022-08-28 12:10:18 +0300

    style the card

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,10 +5,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Container } from "@mui/material";
+import { Avatar, Chip, Container } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCartProducts } from "../../store/cart/cart.selector";
 import { addToCart } from "../../store/cart/cart-action";
+import PaidIcon from "@mui/icons-material/Paid";
+import { Box } from "@mui/system";
 
 export default function MediaCard({ product }) {
   const dispatch = useDispatch();
@@ -25,11 +27,27 @@ export default function MediaCard({ product }) {
         image={product.imageUrl}
         alt={product.name}
       />
-      <CardContent>
+      <CardContent sx={{ position: "relative" }}>
+        <Chip
+          avatar={
+            <Avatar>
+              <PaidIcon />
+            </Avatar>
+          }
+          label={product.price}
+          color="primary"
+          size="medium"
+          sx={{
+            position: "absolute",
+            top: "-1rem",
+            left: "50%",
+            transform: "translate(-50%)",
+          }}
+        />
         <Container maxWidth="lg">
           <Grid container>
             <Grid
-              xs={6}
+              xs={12}
               display="flex"
               justifyContent="center"
               alignItems="center"
@@ -38,21 +56,17 @@ export default function MediaCard({ product }) {
                 {product.name}
               </Typography>
             </Grid>
-            <Grid
-              xs={6}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Typography gutterBottom variant="h5" component="div">
-                ${product.price}
-              </Typography>
-            </Grid>
           </Grid>
         </Container>
       </CardContent>
-      <CardActions>
-        <Button onClick={handleAddToCart} size="small">
+
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Button
+          variant="contained"
+          onClick={handleAddToCart}
+          size="large"
+          sx={{ padding: "1rem" }}
+        >
           Add to cart
         </Button>
       </CardActions>
