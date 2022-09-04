d48c2c17a3dc2d5859b788bb2be409aacd67bc33
Hasan-aga <careerforma@gmail.com>
2022-09-04 18:17:43 +0300

    split bundle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
-import ButtonAppBar from "./components/appbar/appbar.component";
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { checkUserSession } from "./store/user/user.action";
 import { startFetchCategories } from "./store/categories/categories.action";
-import Shop from "./routes/shop/shop.route";
-import SignHomepage from "./routes/sign-in-or-up/sign-homepage.route";
-import Checkout from "./routes/checkout/checkout.route";
-import Homepage from "./routes/homepage/homepage.route";
-import SuccessfulPayment from "./routes/checkout/successful-payment.route";
-import NotFound from "./components/not-found/not-found.component";
+import { Box, CircularProgress } from "@mui/material";
+const ButtonAppBar = lazy(() => import("./components/appbar/appbar.component"));
+const Shop = lazy(() => import("./routes/shop/shop.route"));
+const SignHomepage = lazy(() =>
+  import("./routes/sign-in-or-up/sign-homepage.route")
+);
+const Checkout = lazy(() => import("./routes/checkout/checkout.route"));
+const Homepage = lazy(() => import("./routes/homepage/homepage.route"));
+const SuccessfulPayment = lazy(() =>
+  import("./routes/checkout/successful-payment.route")
+);
+const NotFound = lazy(() =>
+  import("./components/not-found/not-found.component")
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -24,16 +31,27 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<ButtonAppBar />}>
-        <Route index element={<Homepage />}></Route>
-        <Route path="shop/*" element={<Shop />}></Route>
-        <Route path="login/" element={<SignHomepage />}></Route>
-        <Route path="checkout/" element={<Checkout />}></Route>
-        <Route path="success/:message" element={<SuccessfulPayment />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress sx={{ margin: "20% auto", width: "50%" }} />
+        </Box>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<ButtonAppBar />}>
+          <Route index element={<Homepage />}></Route>
+          <Route path="shop/*" element={<Shop />}></Route>
+          <Route path="login/" element={<SignHomepage />}></Route>
+          <Route path="checkout/" element={<Checkout />}></Route>
+          <Route
+            path="success/:message"
+            element={<SuccessfulPayment />}
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
